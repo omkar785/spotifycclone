98e6cf4bdd6e4e3d494cd4eefcbb21eb5eb8574d
omkar785 <omkar.rajurkar7@gmail.com>
2024-05-26 20:15:43 +0530

    layout,basic sidebar setup with Box and initial set up

---
+++ components/Box.tsx
@@ -0,0 +1,21 @@
+import { FiTable } from "react-icons/fi";
+import { twMerge } from "tailwind-merge";
+
+interface BoxProps {
+    children: React.ReactNode;
+    className?: string;
+}
+
+const Box: React.FC<BoxProps>=({
+    children,
+    className
+})=> {
+    return(
+        <div
+        className={twMerge(`bg-neutral-900 rounded-lg h-Fit w-full`,className)}
+        >
+            {children}
+        </div>
+    );
+}
+export default Box;
