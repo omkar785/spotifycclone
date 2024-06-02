5d3875f9780a1e963c44bed6869c8181672481a5
omkar785 <omkar.rajurkar7@gmail.com>
2024-06-02 15:53:59 +0530

    player functional, queue wokring in respective areas, volume slider working

--- components/Library.tsx
+++ components/Library.tsx
@@ -10,6 +10,7 @@ import useUploadModal from "@/hooks/useUploadModal";
 import { Song } from "@/types";
 import React from "react";
 import MediaItem from "./MediaItem";
+import useOnPlay from "@/hooks/useOnPlay";
 
 interface LibraryProps {
     songs: Song[];
@@ -22,6 +23,8 @@ const Library: React.FC<LibraryProps> = ({
     const authModal = useAuthModal();
     const uploadModal = useUploadModal();
     const {user} = useUser();
+
+    const onPlay = useOnPlay(songs);
     
     const onClick =() =>{
         if(!user){
@@ -53,7 +56,7 @@ const Library: React.FC<LibraryProps> = ({
             px-3">
                 {songs.map((item) => (
                     <MediaItem 
-                    onClick={()=>{}}
+                    onClick={(id:string)=>onPlay(id)}
                     key={item.id}
                     data={item}
                     />
