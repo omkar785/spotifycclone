efaf1d532e2bf26546edc2a90c5eb1485740b8cd
omkar785 <omkar.rajurkar7@gmail.com>
2024-06-02 16:19:35 +0530

    loading and error pages added

--- app/search/components/SearchContent.tsx
+++ app/search/components/SearchContent.tsx
@@ -4,6 +4,7 @@ import React from "react";
 import { Song } from "@/types";
 import MediaItem from "@/components/MediaItem";
 import LikeButton from "@/components/LikeButton";
+import useOnPlay from "@/hooks/useOnPlay";
 
 
 interface SearchContentProps {
@@ -11,6 +12,8 @@ interface SearchContentProps {
 }
 
 const SearchContent: React.FC<SearchContentProps> = ({ songs }) => {
+
+  const onPlay = useOnPlay(songs);
 
   if (songs.length === 0) {
     return <div className="flex flex-col gap-y-2 w-full px-6 text-neutral-400">No songs found.</div>;
@@ -20,7 +23,7 @@ const SearchContent: React.FC<SearchContentProps> = ({ songs }) => {
       {songs.map((item) => (
         <div key={item.id} className="flex items-center gap-x-4 w-full">
           <div className="flex-1">
-            <MediaItem onClick={() => {}} data={item} />
+            <MediaItem onClick={(id:string) => onPlay(id)} data={item} />
           </div>
           <LikeButton songId={item.id}/>
         </div>
