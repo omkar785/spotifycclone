97ecce070d15de594d4199608fede9549dc2e48c
omkar785 <omkar.rajurkar7@gmail.com>
2024-06-02 14:45:04 +0530

    ui in player along with functionality to like/dislike in the player itself

--- components/Player.tsx
+++ components/Player.tsx
@@ -3,6 +3,7 @@
 import useGetSongById from "@/hooks/useGetSongById";
 import useLoadSongUrl from "@/hooks/useLoadSongUrl";
 import usePlayer from "@/hooks/usePlayer";
+import PlayerContent from "./PlayerContent";
 
 const Player = () => {
 
@@ -27,7 +28,11 @@ const Player = () => {
         px-4
         "
         > 
-        <h1>Player</h1>
+        <PlayerContent 
+        key={songUrl}
+        song={song}
+        songUrl={songUrl}
+        />
         </div>
     );
 }
